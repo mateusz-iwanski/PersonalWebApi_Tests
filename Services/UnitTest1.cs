2a2932ecf6479d48aca4003130cdab1a70b04e94
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-11 13:50:19 +0100

    init

---
+++ Services/UnitTest1.cs
@@ -0,0 +1,120 @@
+﻿using Xunit;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using PersonalWebApi.Agent;
+using System.Collections.Generic;
+using Microsoft.KernelMemory;
+using Microsoft.SemanticKernel;
+using PersonalWebApi.Tests.Controllers.Agent;
+
+namespace PersonalWebApi.Tests.Services
+{
+    public class AgentTests
+    {
+        private readonly TestConfiguration _testConfig;
+
+        public AgentTests()
+        {
+            _testConfig = new TestConfiguration();
+        }
+
+        [Fact]
+        public async Task Chat_ReturnsExpectedResult()
+        {
+            // Arrange
+            string conversationUuid = "30f4373b-5b18-41fd-8b40-5953825b3c0d";
+            int id = 1;
+            var sessionId = Guid.NewGuid().ToString();
+
+            // Ensure the file exists for the test
+            string filePath = Path.Combine(AppContext.BaseDirectory, "bajka.docx");
+            if (!File.Exists(filePath))
+            {
+                await File.WriteAllTextAsync(filePath, "This is a test document content.");
+            }
+
+            // Name of the plugin. This is the name you'll use in skPrompt, e.g. {{memory.ask ...}}
+            var pluginName = "memory";
+
+            TagCollection conversation_1_id_tags = new TagCollection();
+            conversation_1_id_tags.Add("sessionUuid", Guid.NewGuid().ToString());
+
+            var conversationId = Guid.Parse(conversationUuid);
+
+            var memoryPlugin = _testConfig.Kernel.ImportPluginFromObject(
+                new MemoryPlugin(_testConfig.Memory, waitForIngestionToComplete: true),
+                pluginName);
+
+            var skPrompt = """
+                        Question to Memory: {{$input}}
+
+                        Answer from Memory: {{memory.ask $input index=$index}}
+
+                        If the answer is empty look forward. If you find answer say 'I haven't in memory but ai found the answer - <answer>' otherwise reply with a preview of the answer,
+                        truncated to 15 words. Prefix with one emoji relevant to the content.
+                        """;
+
+            var f = new PromptExecutionSettings()
+            {
+                ExtensionData = new Dictionary<string, object>()
+                {
+                    { "conversationUuid", conversationUuid },
+                    { "sessionUuid", sessionId },
+                    { "shortMemory", true }
+                }
+            };
+
+            OpenAIPromptExecutionSettings settings = new()
+            {
+                ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
+            };
+
+            KernelArguments arguments = new KernelArguments(settings)
+            {
+                ["input"] = "Kto skręcił sobie nogę?",
+                ["index"] = conversationId.ToString(),
+                ["conversationUuid"] = conversationUuid,
+                ["sessionUuid"] = sessionId
+            };
+
+            var prompts = _testConfig.Kernel.CreatePluginFromPromptDirectory(Path.Combine(AppContext.BaseDirectory, "../../../Agent/Prompts"));
+
+            var chatResult = _testConfig.Kernel.InvokeStreamingAsync<StreamingChatMessageContent>(
+              prompts["Complaint"],
+              new()
+              {
+               { "request", "my pdf filter is faulty" },
+               { "input" , "Kto skręcił sobie nogę?" },
+               { "conversationUuid", conversationUuid },
+               { "sessionUuid", sessionId }
+              }
+            );
+
+            string message = "";
+
+            await foreach (var chunk in chatResult)
+            {
+                if (chunk.Role.HasValue)
+                {
+                    Console.Write(chunk.Role + " > ");
+                }
+                message += chunk;
+                Console.Write(chunk);
+            }
+
+            Console.WriteLine();
+
+            var myFunction = _testConfig.Kernel.CreateFunctionFromPrompt(skPrompt, f);
+
+            var answer = await myFunction.InvokeAsync(_testConfig.Kernel, arguments);
+
+            // Assert
+            Assert.NotNull(answer);
+            Assert.True(answer.ToString().Length > 0);
+            Assert.IsType<string>(answer.ToString());
+        }
+    }
+}
---
+++ TestConfiguration.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PersonalWebApi.Agent.MicrosoftKernelMemory;
+using PersonalWebApi.Services.Azure;
+using PersonalWebApi.Utilities.Utilities.DocumentReaders;
+using PersonalWebApi.Services.Services.Qdrant;
+using Microsoft.SemanticKernel;
+using Microsoft.Extensions.Logging;
+using PersonalWebApi.Agent.SemanticKernel.Observability;
+using PersonalWebApi.Exceptions;
+using PersonalWebApi.Services.Services.Agent;
+using PersonalWebApi.Services.System;
+using PersonalWebApi.Utilities.Utilities.Qdrant;
+using PersonalWebApi.Extensions;
+using Microsoft.AspNetCore.Identity;
+using PersonalWebApi.Utilities.Utilities.HttUtils;
+using Microsoft.KernelMemory;
+using PersonalWebApi.Agent;
+
+namespace PersonalWebApi.Tests.Controllers.Agent
+{
+    public class TestConfiguration
+    {
+        public ServiceProvider ServiceProvider { get; private set; }
+        public Kernel Kernel { get; private set; }
+        public KernelMemoryWrapper Memory { get; private set; }
+        public IBlobStorageService BlobStorage { get; private set; }
+        public IDocumentReaderDocx DocumentReaderDocx { get; private set; }
+        public IQdrantFileService Qdrant { get; private set; }
+        public IConfiguration Configuration { get; private set; }
+        public IAssistantHistoryManager AssistantHistoryManager { get; private set; }
+
+        public TestConfiguration()
+        {
+            // Set up the service collection
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddHttpContextAccessor();
+
+            // Add configuration
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Nlog.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.OpenAi.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.Telemetry.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.Qdrant.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.Azure.json", optional: true, reloadOnChange: true)
+                .AddUserSecrets<Program>()
+                .AddEnvironmentVariables()
+                .Build(); 
+            serviceCollection.AddSingleton<IConfiguration>(configuration);
+
+            // Manually register services
+            serviceCollection.AddScoped<Kernel>(sp =>
+            {
+                var kernelBuilder = Kernel.CreateBuilder();
+
+                var apiKey = configuration.GetSection("OpenAI:Access:ApiKey").Value ??
+                    throw new SettingsException("OpenAi ApiKey not exists in appsettings");
+
+                var defaultModelId = configuration.GetSection("OpenAI:DefaultModelId").Value ??
+                    throw new SettingsException("OpenAi DefaultModelId not exists in appsettings");
+
+                kernelBuilder.AddOpenAIChatCompletion(
+                    defaultModelId,
+                    apiKey
+                );
+
+                // Use the correct method to add logging
+                kernelBuilder.Services.AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.AddConsole();
+                });
+
+                kernelBuilder.Services.AddSingleton<IConfiguration>(configuration);
+
+                // Register IHttpContextAccessor early
+                kernelBuilder.Services.AddHttpContextAccessor();
+
+                kernelBuilder.Services.AddScoped<ICosmosDbService, AzureCosmosDbService>();
+                kernelBuilder.Services.AddScoped<IAssistantHistoryManager, AssistantHistoryManager>();
+                kernelBuilder.Services.AddScoped<IPromptRenderFilter, RenderedPromptFilterHandler>();
+
+                IKernelMemory memory = new KernelMemoryBuilder()
+                    .WithOpenAIDefaults(apiKey)
+                    .Build<MemoryServerless>();
+
+                kernelBuilder.Services.AddScoped<IKernelMemory>(_ => memory);
+
+                kernelBuilder.Services.AddScoped<KernelMemoryWrapper>(provider =>
+                {
+                    var innerKernelMemory = provider.GetRequiredService<IKernelMemory>();
+                    var assistantHistoryManager = provider.GetRequiredService<IAssistantHistoryManager>();
+
+                    return new KernelMemoryWrapper(innerKernelMemory, assistantHistoryManager);
+                });
+
+                return kernelBuilder.Build();
+            });
+
+            serviceCollection.AddScoped<IKernelMemory>(sp =>
+            {
+                var apiKey = configuration.GetSection("OpenAI:Access:ApiKey").Value ?? throw new SettingsException("OpenAi ApiKey not exists in appsettings");
+                return new KernelMemoryBuilder().WithOpenAIDefaults(apiKey).Build<MemoryServerless>();
+            });
+
+            serviceCollection.AddScoped<IAssistantHistoryManager, AssistantHistoryManager>();
+
+            serviceCollection.AddScoped<KernelMemoryWrapper>(provider =>
+            {
+                var innerKernelMemory = provider.GetRequiredService<IKernelMemory>();
+                var assistantHistoryManager = provider.GetRequiredService<IAssistantHistoryManager>();
+
+                return new KernelMemoryWrapper(innerKernelMemory, assistantHistoryManager);
+            });
+
+            // Register other necessary services
+            serviceCollection.AddScoped<IAccountService, AccountService>();
+            serviceCollection.AddScoped<IBlobStorageService, AzureBlobStorageService>();
+            serviceCollection.AddScoped<IDocumentReaderDocx, DocumentReaderDocx>();
+            serviceCollection.AddScoped<IQdrantFileService, QdrantFileService>();
+            serviceCollection.AddScoped<QdrantRestApiClient>();
+            serviceCollection.AddScoped<IEmbedding, EmbeddingOpenAi>();
+            serviceCollection.AddScoped<ICosmosDbService, AzureCosmosDbService>();
+            serviceCollection.AddScoped<IApiClient, ApiClient>();
+
+            // Build the service provider
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            // Resolve the dependencies
+            Kernel = ServiceProvider.GetRequiredService<Kernel>();
+            Memory = ServiceProvider.GetRequiredService<KernelMemoryWrapper>();
+            BlobStorage = ServiceProvider.GetRequiredService<IBlobStorageService>();
+            DocumentReaderDocx = ServiceProvider.GetRequiredService<IDocumentReaderDocx>();
+            Qdrant = ServiceProvider.GetRequiredService<IQdrantFileService>();
+            Configuration = ServiceProvider.GetRequiredService<IConfiguration>();
+            AssistantHistoryManager = ServiceProvider.GetRequiredService<IAssistantHistoryManager>();
+        }
+    }
+}
