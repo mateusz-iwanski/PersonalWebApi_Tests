59e0d9020b2b6a1a308207438bd128be900e8732
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-13 20:24:05 +0100

    Add Output class for formatted logging in tests
    Introduce a new static class `Output` in `PersonalWebApi.Tests` namespace.
    The `Output` class includes a `Write` method that formats messages with
    a decorative border and a custom log prefix. The formatted messages are
    output to both the debug output and the console. Added necessary `using`
    directives for required namespaces.

---
+++ Output.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalWebApi.Tests
+{
+    public static class Output
+    {
+        public static void Write(string message)
+        {
+            string fancySign = new string('*', 50);
+            Debug.WriteLine($"{fancySign}\n\t#### MyLog| {message}");
+            Console.WriteLine($"{fancySign}\n\t#### MyLog| {message}");
+        }
+    }
+}
