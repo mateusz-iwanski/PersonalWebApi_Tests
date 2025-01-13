c0650b53c75f33cf2bd2164541f0cc34a4bcb375
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-13 20:23:54 +0100

    Add AzureBlobPlugin tests and configuration setup
    Introduce AzurePluginsTest class to test AzureBlobPlugin functionality.
    Add necessary using directives for configuration and services.
    Initialize TestConfiguration and IConfiguration in the constructor.
    Implement AzureBlobPlugin_downloadFile test method:
    - Retrieve OpenAI API key and model ID from configuration.
    - Configure kernel builder with OpenAI chat completion.
    - Load JSON configuration files and environment variables.
    - Add AzureBlobStorageService and configuration to services.
    - Add AzureBlobPlugin to kernel builder's plugins.
    - Invoke prompts on the kernel with and without the plugin.

---
+++ Services/Plugins/AzurePluginsTest.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using PersonalWebApi.Agent.SemanticKernel.Plugins.StoragePlugins.AzureBlob;
+using PersonalWebApi.Exceptions;
+using PersonalWebApi.Services.Azure;
+using PersonalWebApi.Tests.Controllers.Agent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalWebApi.Tests.Services.Plugins
+{
+    public class AzurePluginsTest
+    {
+        private readonly TestConfiguration _testConfig;
+        private readonly IConfiguration _configuration;
+        public AzurePluginsTest()
+        {
+            _testConfig = new TestConfiguration();
+            _configuration = _testConfig.Configuration;
+        }
+
+        [Fact]
+        public async Task AzureBlobPlugin_downloadFile()
+        {
+            // kernel settings
+
+            var apiKey = _configuration.GetSection("OpenAI:Access:ApiKey").Value ??
+                    throw new SettingsException("OpenAi ApiKey not exists in appsettings");
+
+            var defaultModelId = _configuration.GetSection("OpenAI:DefaultModelId").Value ??
+                throw new SettingsException("OpenAi DefaultModelId not exists in appsettings");
+
+            // Create a kernel with OpenAI chat completion
+            IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+            kernelBuilder.AddOpenAIChatCompletion(
+                    modelId: defaultModelId,
+                    apiKey: apiKey
+                    );
+
+            var configuration = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.Nlog.json", optional: true, reloadOnChange: true)
+               .AddJsonFile("appsettings.OpenAi.json", optional: true, reloadOnChange: true)
+               .AddJsonFile("appsettings.Telemetry.json", optional: true, reloadOnChange: true)
+               .AddJsonFile("appsettings.Qdrant.json", optional: true, reloadOnChange: true)
+               .AddJsonFile("appsettings.Azure.json", optional: true, reloadOnChange: true)
+               .AddUserSecrets<Program>()
+               .AddEnvironmentVariables()
+               .Build();
+
+            // DI
+            kernelBuilder.Services.AddScoped<IBlobStorageService, AzureBlobStorageService>();
+            kernelBuilder.Services.AddSingleton<IConfiguration>(configuration);
+
+            // add plugin
+
+            kernelBuilder.Plugins.AddFromType<AzureBlobPlugin>();
+            Kernel kernel = kernelBuilder.Build();
+
+            // without plugin
+            var result = await kernel.InvokePromptAsync("Pokaż jakie kontenery są na blob azure?");
+
+            Output.Write(result.ToString());
+
+            // with plugin
+            OpenAIPromptExecutionSettings settings = new() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+            var resultPlugin = await kernel.InvokePromptAsync("Pokaż jakie kontenery są na blob azure?", new(settings));
+
+            Output.Write(resultPlugin.ToString());
+        }
+    }
+}
