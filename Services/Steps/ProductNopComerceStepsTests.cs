943b68d1b03529e10e638f6ac104c241de50e383
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-02-25 12:37:25 +0100

    Add experimental test class for ProductNopComerce steps
    Introduce `ProductNopComerceStepsTests` in `PersonalWebApi.Tests.Services.Steps` namespace with dependencies on various libraries. Add private readonly `_testConfig` field and initialize it in the constructor. Implement `ProductParaphrase_SouldFillProductNopStepDto` unit test method using `[Fact]` attribute. Mark constructor and test method as experimental with `[Experimental("SKEXP0080")]` attribute.

---
+++ Services/Steps/ProductNopComerceStepsTests.cs
@@ -0,0 +1,46 @@
+﻿using iText.Commons.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.SemanticKernel;
+using PersonalWebApi.Processes.Document.Models;
+using PersonalWebApi.Processes.FileStorage.Processes;
+using PersonalWebApi.Processes.NopCommerce.Models;
+using PersonalWebApi.Processes.NopCommerce.Processes;
+using PersonalWebApi.Processes.Qdrant.Pipelines;
+using PersonalWebApi.Tests.Controllers.Agent;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalWebApi.Tests.Services.Steps
+{
+    public class ProductNopComerceStepsTests
+    {
+
+        private readonly TestConfiguration _testConfig;
+
+        [Experimental("SKEXP0080")]
+        public ProductNopComerceStepsTests()
+        {
+            _testConfig = new TestConfiguration();
+        }
+
+        [Fact]
+        [Experimental("SKEXP0080")]
+        public async Task ProductParaphrase_SouldFillProductNopStepDto()
+        {
+            var productNopStepDto = new ProductCollectNopStepDto()
+            {
+                Sku = "BL1"
+            };
+
+            CollectDataProcess collectDataProcess = new CollectDataProcess();
+            ProductNopPipelines pipeline = new ProductNopPipelines();
+            await pipeline.CollectProductPipeline(ProductNopPipelines.PrepareKelnerForPipeline(_testConfig.Configuration), productNopStepDto);
+        }
+
+    }
+}
