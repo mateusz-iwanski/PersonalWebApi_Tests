ff08e3890a2045e8876a6e74dc46e0fc780433df
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-02-28 11:19:25 +0100

    Add NopCommercePluginTests with async test method
    Introduced `NopCommercePluginTests` class in `PersonalWebApi.Tests.Services.Plugins` namespace. Added using directives for necessary namespaces. Implemented private fields `_testConfig` and `_configuration` with a constructor initializing them. Marked the constructor with `[Experimental("SKEXP0050")]`. Added `ParaphraseTitlePlugin` async test method to test NopCommerce plugin integration.

---
+++ Services/Plugins/NopCommercePluginTests.cs
@@ -0,0 +1,41 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using PersonalWebApi.Agent.SemanticKernel.Plugins.NopCommerce;
+using PersonalWebApi.Tests.Controllers.Agent;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalWebApi.Tests.Services.Plugins
+{
+    public class NopCommercePluginTests
+    {
+        private readonly TestConfiguration _testConfig;
+        private readonly IConfiguration _configuration;
+
+        [Experimental("SKEXP0050")]
+        public NopCommercePluginTests()
+        {
+            _testConfig = new TestConfiguration();
+            _configuration = _testConfig.Configuration;
+        }
+
+        [Fact]
+        public async Task ParaphraseTitlePlugin()
+        {
+            // Arrange
+            _testConfig.Kernel.Plugins.AddFromType<NopCommercePlugin>();
+            
+            // Act
+            // with plugin
+            OpenAIPromptExecutionSettings settings = new() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+            var resultPlugin = await _testConfig.Kernel.InvokePromptAsync("Parafrazuj produkt BL1", new(settings));
+            Output.Write(resultPlugin.ToString());
+        }
+    }
+}
